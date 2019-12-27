893391891a3919c610e693a3c6936f9f9038b992
Rima-cpu <55252887+Rima-cpu@users.noreply.github.com>
2019-12-27 10:32:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include "reponse.h"
-#include "question.h"
-#include <string>
-#include <fstream>
-#include <iostream>
-using namespace QCMpp;
 
 int main(int argc, char *argv[])
 {
-//    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-//    return a.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 
-    Reponse r1("first choice", true);
-    Reponse r2("second choice",false);
-    Reponse r3("thrid choice", false);
-    Question q1("Reponse?");
-    q1.add_choice(r1);
-    q1.add_choice(r2);
-    q1.add_choice(r3);
-
-
-
+    
     return 0;
 
 }
