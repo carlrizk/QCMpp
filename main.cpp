f85f60fd2e0de161a6effc1427a015135a7be68f
Carl Rizk <carlrizk88@gmail.com>
2019-12-27 16:58:53 +0200

    Fixing main

--- main.cpp
+++ main.cpp
@@ -4,30 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
-//    QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-//    return a.exec();
-
-    Reponse r1("first choice", true);
-    Reponse r2("second choice",false);
-    Reponse r3("thrid choice", false);
-    Question q1("Reponse?");
-    q1.add_choice(r1);
-    q1.add_choice(r2);
-    q1.add_choice(r3);
-
-    Reponse r5;
-
-=======
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
->>>>>>> upstream/master
-
-    
-    return 0;
-
+   QApplication a(argc, argv);
+   MainWindow w;
+   w.show();
+   return a.exec();
 }
