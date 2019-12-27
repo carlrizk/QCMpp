24a7e71f9e4bca0c1f395d44d81ba909919a389e
Rima Al Ghossein <rimaalghossein@gmail.com>
2019-12-27 14:35:45 +0200

    Enhancements on Question and Answer Classes

--- answer.h
+++ answer.h
@@ -7,12 +7,12 @@ class Answer
 {
 public:
 
-    Answer(const std::string& texte, const bool correct);
+    Answer(const std::string& text, const bool correct);
 
     bool isCorrect() const;
 
 private:
-    std::string texte;
+    std::string text;
     bool correct;
 };
 }
